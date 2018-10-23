9ac706f721a3b8ef58baf4b6e9b7921fe7a223cb
Nar Cuenca <narc.ph@gmail.com>
2018-10-23 13:25:43 +0800

    migrated database to mongoDB Atlas

--- app.js
+++ app.js
@@ -1,56 +1,67 @@
-require('./config/config');
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+require("./config/config");
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
-var index = require('./routes/index');
-var user = require('./routes/user');
-var leave = require('./routes/leave');
-var admin = require('./routes/admin');
-var cronLeave = require('./utils/cronLeaveCredit');
+var index = require("./routes/index");
+var user = require("./routes/user");
+var leave = require("./routes/leave");
+var admin = require("./routes/admin");
+var cronLeave = require("./utils/cronLeaveCredit");
 
 var app = express();
 cronLeave.start();
 
-mongoose.connect(process.env.MONGODB_URI).then(
-  () => {
-    console.log('Now connected to mongoDB');
-  },
-  (err) => console.error(err)
-);
+mongoose
+  .connect(
+    process.env.MONGODB_URI,
+    { useNewUrlParser: true }
+  )
+  .then(
+    () => {
+      console.log("Now connected to mongoDB");
+    },
+    err => console.error(err)
+  );
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // CORS
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-auth');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-auth"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST, GET, PATCH, DELETE, OPTIONS"
+  );
   next();
-})
+});
 
-app.use('/', index);
-app.use('/user', user);
-app.use('/leave', leave);
-app.use('/admin', admin);
+app.use("/", index);
+app.use("/user", user);
+app.use("/leave", leave);
+app.use("/admin", admin);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -59,8 +70,8 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   if (res.headersSent) return next(err);
   res.status(err.status || 500).json({
-    message: err.message || 'Shoot! An error has occurred.',
-    errorStack: err,
+    message: err.message || "Shoot! An error has occurred.",
+    errorStack: err
   });
 });
 
