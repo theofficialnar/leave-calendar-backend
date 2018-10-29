024dfb0992dbbc1ccb703cac6bf0a9dc9e44f615
Nar Cuenca <narc.ph@gmail.com>
2018-10-29 14:24:43 +0800

    updated admin route tests

--- tests/env-test.js
+++ tests/env-test.js
@@ -1,7 +1,8 @@
 module.exports = {
   test: describe("env: check", () => {
-    it('should return "test" as NODE_ENV', () => {
+    it('should return "test" as NODE_ENV', done => {
       expect(process.env.NODE_ENV).toBe("test");
+      done();
     });
   })
 };
